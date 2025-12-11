a65af1a638496a11e3711caaf90ac4e01b5074fe
Andres Ghersi <andres.ghersi@gmail.com>
2025-12-10 20:51:42 -0500

    Added token swagger option

---
+++ src/main/java/com/inferia/backendinferia/config/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package com.inferia.backendinferia.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Inferia Backend API",
+                version = "1.0",
+                description = "Documentation for backend API, including endpoints and resources"
+        ),
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        description = "Enter JWT token (including prefix. Example: Bearer <token> )"
+)
+@Configuration
+public class OpenApiConfig {
+}
